7dcd0dcaac4a0a87e83f9e5e997249a380c71a14
PatrykZymelka <127216593+PatrykZymelka@users.noreply.github.com>
2023-03-09 19:50:11 +0100

    Update Core.py

--- Core.py
+++ Core.py
@@ -17,4 +17,4 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run('NDk1MjYxMTI0ODY3MTI5Mzc0.GYXA6N.vrmAdStChgVjHyAPoZqwR_WrbG_12Gr3TO2Pxg')
+client.run('Token')
